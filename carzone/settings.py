2694f46a7e05799c67a95ec90a42025c848ed2b3
Grant-Uzu <grantuzu07@gmail.com>
2022-05-31 14:54:59 +0100

    copleted Teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'COMPUTER&boy2',
+        'HOST': 'localhost',
     }
 }
 
@@ -124,9 +127,8 @@ STATICFILES_DIRS = [
 ]
 
 #media settings
-MEDIA_URL = '/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
